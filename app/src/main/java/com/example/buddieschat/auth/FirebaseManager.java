95ede9c0ce847748cbe18f292fa0759215bfbf26
prdimitrov <prdimitrov97@gmail.com>
2025-01-02 14:21:04 +0200

    - Add FirebaseManager.java class, to handle the authentication with Google Firebase - Add GroupsActivity.java class.

---
+++ app/src/main/java/com/example/buddieschat/auth/FirebaseManager.java
@@ -0,0 +1,51 @@
+package com.example.buddieschat.auth;
+
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+
+import com.example.buddieschat.views.GroupsActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class FirebaseManager {
+
+    /**
+     * This method does authenticate the anonymous user, that will log in to the application
+     * @param context
+     */
+    public void firebaseAnonymousAuth(Context context) {
+        FirebaseAuth.getInstance().signInAnonymously().addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+                    // If Auth is successful
+                    Intent i = new Intent(context, GroupsActivity.class);
+                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(i);
+
+                }
+            }
+        });
+    }
+
+    /**
+     *
+     * This method returns the ID of the current user
+     */
+    public String getCurrentUserId() {
+        return FirebaseAuth.getInstance().getUid();
+    }
+
+    /**
+     *
+     * This method is used to sign out the user from the application
+     */
+    public void signOut() {
+        FirebaseAuth.getInstance().signOut();
+    }
+}
